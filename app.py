28fe8d7b191f9b56af1350f7c356c279ac9bf8d3
David Lin <David@D-10-65-16-242.dhcp4.washington.edu>
2016-08-27 15:04:58 -0700

    Added external endpoint.

--- app.py
+++ app.py
@@ -5,13 +5,22 @@ app = Flask(__name__)
 ACCOUNT_SID = "ACe6cf9fbeb5dd24c243eb287285f19b03"
 AUTH_TOKEN = "2fb9596882434b38e285c65a412698bc"
 
-@app.route('/submit',methods=['POST','GET'])
-def textme():
+def textNumber(number, location):
+	response = "Hello, the closest location to get healthcare would be " + location
+	number = "+1" + number
 	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
-	message = client.messages.create(to="+19167170483", from_="+19169707789",
-                                     body="Hi Xtina its david im texting you from twilio!")
-	return
+	try:
+		message = client.messages.create(to=number, from_="+19169707789", body=response)
+	except Exception as e:
+		return 'Failed'
+	return 'Success'
+
+
+
+@app.route('/test',methods=['POST','GET'])
+def textme():
+	return textNumber("9167170483","Seattle Grace Hospital")
 
 
 if __name__ == "__main__":
-	app.run(debug = True)
+	app.run(host='0.0.0.0', port = 80, debug = True)
